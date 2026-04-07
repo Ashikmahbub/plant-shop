c53f9b43b4ecec425cf9fbf6ba199fcaa1854f4f
ashikmahbub56 <ashikmahbub56@gmail.com>
2026-04-07 21:21:16 +0600

    redis added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Banner from "../../components/ui/Banner";
 import Bonsai from "../Categories/Bonsai";
 import Flower from "../Categories/Flower";
@@ -8,27 +8,38 @@ import Outdoor from "../Categories/Outdoor";
 import Packages from "../Categories/Packages";
 import SemiIndoor from "../Categories/SemiIndoors";
 
+const CategorySection = ({ to, children }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate(to)}
+      className="cursor-pointer hover:opacity-90 transition-opacity duration-200"
+    >
+      {children}
+    </div>
+  );
+};
+
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <Banner />
       <div className="text-center mt-10">
-        <Link
-          to="/shop"
+        <button
+          onClick={() => navigate('/shop')}
           className="bg-green-600 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-green-700 transition duration-300"
         >
           Shop Now
-        </Link>
+        </button>
       </div>
-      <Indoor />
-      <SemiIndoor />
-      <Flower />
-      <Bonsai />
-      <Outdoor />
-      <Packages />
 
-      {/* Shop Now Button */}
-    
+      <CategorySection to="/indoor">       <Indoor />     </CategorySection>
+      <CategorySection to="/semi-indoor">  <SemiIndoor /> </CategorySection>
+      <CategorySection to="/shop?category=flower">  <Flower />    </CategorySection>
+      <CategorySection to="/shop?category=bonsai">  <Bonsai />    </CategorySection>
+      <CategorySection to="/outdoor">      <Outdoor />    </CategorySection>
+      <CategorySection to="/shop?category=packages"><Packages />  </CategorySection>
     </div>
   );
 };
